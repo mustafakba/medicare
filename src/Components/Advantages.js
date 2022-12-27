64726301cb3ab2dc5255729514baeeed49ee9b95
mustafakba <kaba.mustafaa@gmail.com>
2022-12-27 23:00:14 +0300

    Advantages js files to be dynamic

--- src/Components/Advantages.js
+++ src/Components/Advantages.js
@@ -1,12 +1,13 @@
 import styles from '../Styles/Advantages.css'
 import {Icon} from "./Icons";
 import {BrowserRouter as Router} from 'react-router-dom'
-import {Switch , Route} from "react-router-dom";
-import {useState} from "react";
 import DarkButton from "./Shared/DarkButton";
-import WhiteButton from "./Shared/WhiteButton";
+import {useState} from 'react'
 
 function Advantages() {
+    const [activeIndex, setActiveIndex] = useState(0)
+    const items = [1,2,3,4,5];
+    const contents = ["lorem ipsum","dolor sit", "amet" , "deneme" ,"memo"]
     return  (
        <Router>
            <div className={styles}>
@@ -22,60 +23,32 @@ function Advantages() {
                            <div className="advantages-box">
                                <div className="advantages-router">
                                    <div className="advantages-bottom-container">
-                                       <div className="advantages-item">
-                                               <div className="advantages-box active">
-                                                   <div className="advantages-box-content">
-                                                       <div className="advantages-box-content-icon">
-                                                           <span>I</span>
+                                       {
+                                           items.map((item,index) =>{
+                                               return(
+                                                   <div key={index} onClick={ () => setActiveIndex(index) }  className={activeIndex === index ? 'advantages-item active': 'advantages-item' }>
+                                                       <div className="advantages-box">
+                                                           <div className={activeIndex === index ? 'advantages-box-content active': 'advantages-box-content' }>
+                                                               <div className="advantages-box-content-icon">
+                                                                   <div>
+                                                                       {index + 1}
+                                                                   </div>
+                                                               </div>
+                                                           </div>
                                                        </div>
                                                    </div>
-                                               </div>
-                                       </div>
-                                       <div className="advantages-item">
-                                           <div className="advantages-box">
-                                               <div className="advantages-box-content">
-                                                   <div className="advantages-box-content-icon">
-                                                       <span>II</span>
-                                                   </div>
-                                               </div>
-                                           </div>
-                                       </div>
-                                       <div className="advantages-item">
-                                           <div className="advantages-box">
-                                               <div className="advantages-box-content">
-                                                   <div className="advantages-box-content-icon">
-                                                       <span>III</span>
-                                                   </div>
-                                               </div>
-                                           </div>
-                                       </div>
-                                       <div className="advantages-item">
-                                           <div className="advantages-box">
-                                               <div className="advantages-box-content">
-                                                   <div className="advantages-box-content-icon">
-                                                       <span>IV</span>
-                                                   </div>
-                                               </div>
-                                           </div>
-                                       </div>
-                                       <div className="advantages-item">
-                                           <div className="advantages-box">
-                                               <div className="advantages-box-content">
-                                                   <div className="advantages-box-content-icon">
-                                                       <span>V</span>
-                                                   </div>
-                                               </div>
-                                           </div>
-                                       </div>
+                                               )
+                                           })
+                                       }
                                    </div>
                                </div>
                                <div className="advantages-title">
                                    QUALIFIED CONSULTING
                                </div>
                                <div className="advantages-explanation">
-                                    <p>
-                                        Lorem ipsum sit amet, consectetur adipiscing elit. Aenean ullamcorper non lectus eu posuere. Cras dapibus lacus eu varius. In in scelerisque nunc, non vulputate felis. Cras at sagittis lorem lorem.
-                                    </p>
+                                   {
+                                       contents[activeIndex]
+                                   }
                                </div>
                                <div className="advantage-button-area">
                                    <span style={{marginLeft:'35px'}}><DarkButton text="VIEW DETAIL" iconName="right"/></span>
