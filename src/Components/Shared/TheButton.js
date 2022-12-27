16ee29e7b71d175a98dc1b303c3d3809f5fc3ad3
mustafakba <kaba.mustafaa@gmail.com>
2022-12-27 12:19:02 +0300

    ButtonJs dinamik hale getirildi

--- src/Components/Shared/TheButton.js
+++ src/Components/Shared/TheButton.js
@@ -1,15 +1,16 @@
 import {RiPencilFill} from "react-icons/ri";
 import styles from '../../Styles/TheButton.css'
+import {Icon} from '../Icons'
 
-function TheButton() {
+function TheButton({text,iconName}) {
     return (
         <div className={styles}>
             <div className="button-side">
                 <span className="button-left-side">
-                    APPOINTMENT
+                    {text}
                 </span>
                 <span className="button-right-side">
-                    <RiPencilFill/>
+                    <Icon name={iconName} size={12}/>
                 </span>
             </div>
         </div>
