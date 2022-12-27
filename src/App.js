ddaa1cdd4faa76f4f18f8b5e7ca11b0c843ec792
mustafakba <kaba.mustafaa@gmail.com>
2022-12-27 16:14:38 +0300

    WhoWeAre Component created and finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './style.css'
 import Header from './Components/Shared/Header'
+import Slider from "./Components/Slider";
+import WeAre from "./Components/WeAre";
 
 function App() {
   return (
     <div style={{margin:0, padding:0, fontSize:'100%',font : 'inherit' }} className="App">
-      <Header></Header>
+        {/*  Header section start*/}
+            <Header></Header>
+        {/*  Header section finished*/}
+
+        {/*  Slider section start*/}
+            <Slider></Slider>
+         {/* Slider Section finished*/}
+
+        {/*  WhoWeAre section start*/}
+            <WeAre></WeAre>
+         {/* WhoWeAre Section finished*/}
+
+
+
     </div>
   );
 }
---
+++ src/Components/WeAre.js
@@ -0,0 +1,101 @@
+import styles from '../Styles/WeAre.css'
+import DarkButton from "./Shared/DarkButton";
+import {Icon} from "./Icons";
+
+
+function WeAre() {
+    return (
+        <div className={styles}>
+          <div className="container-we-are">
+              <div className="top-section">
+                  <div className="left-section">
+                      <div className="border">
+                          <img src="https://themesflat.com/html/medicare/images/blog/about.jpg" alt=""/>
+                      </div>
+                  </div>
+                  <div className="right-section">
+                      <h5 className="welcome-section">WELCOME TO MEDICARE</h5>
+                      <div className="we-are-title">
+                          WHO WE ARE
+                      </div>
+                      <div className="we-are-title-line">
+                      </div>
+                      <p className="we-are-paragraph">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing. Nunc pharetra interdum iaculis. <br/> Vestibulum semper mauris felis, euismod velit consequat sed. Proin porttitor purus in <br/> odio egestas, sed efficitur nibh ultrices.
+                      </p>
+                      <p className="we-are-paragraph">
+                          Morbi mattis sollicitudin libero ut finibus. Morbi imperdiet tortor ex, ut convallis nisi vestibulum eget. Phasellus hendrerit ut erat eget pharetra.
+                      </p>
+                      <div style={{maxWidth: '24%', flexWrap:'nowrap', marginTop: '50px'}}>
+                          <DarkButton text="OUR TEAM" iconName="user"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="we-are-bottom-section">
+                  <div className="we-are-bottom-container">
+                      <div className="item">
+                          <div className="box">
+                              <div className="box-content">
+                                <div className="box-content-icon">
+                                    <Icon name="user" size={24}/>
+                                </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="numbers">
+                          <div className="we-are-bottom-number">3928</div>
+                          <div className="we-are-bottom-explanation"> Happy Clients</div>
+                      </div>
+                  </div>
+                  <div className="we-are-bottom-container">
+                      <div className="item">
+                          <div className="box">
+                              <div className="box-content">
+                                  <div className="box-content-icon">
+                                      <Icon name="plaster" size={24}/>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="numbers">
+                          <div className="we-are-bottom-number">650</div>
+                          <div className="we-are-bottom-explanation">Equipment</div>
+                      </div>
+                  </div>
+                  <div className="we-are-bottom-container">
+                      <div className="item">
+                          <div className="box">
+                              <div className="box-content">
+                                  <div className="box-content-icon">
+                                     <Icon name="like" size={24}/>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="numbers">
+                          <div className="we-are-bottom-number">3928</div>
+                          <div className="we-are-bottom-explanation"> Happy Clients</div>
+                      </div>
+                  </div>
+                  <div className="we-are-bottom-container">
+                      <div className="item">
+                          <div className="box">
+                              <div className="box-content">
+                                  <div className="box-content-icon">
+                                      <Icon name="heart" size={24}/>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className="numbers">
+                          <div className="we-are-bottom-number">55</div>
+                          <div className="we-are-bottom-explanation"> Our Doctor</div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+    );
+}
+
+export default WeAre;
