36a163d3dc4e71b8c028b9ffce443447df6b4cb9
mustafakba <kaba.mustafaa@gmail.com>
2022-12-27 14:12:06 +0300

    RightArrowIcon created from Iconjs

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -7,10 +7,32 @@ const PencilIcon =({size}) =>{
        </svg>
     )
 }
+const ArrowIcon =({size}) =>{
+    return(
+       <svg viewBox="0 0 24 24" strokeWidth="0" width={size} height={size} xmlns="http://www.w3.org/2000/svg">
+           <path
+               strokeWidth="2"
+               d="M2,12 L22,12 M13,3 L22,12 L13,21"
+               fill="currentColor"/>
+       </svg>
+    )
+}
+const rightIcon =({size}) =>{
+    return(
+       <svg viewBox="0 0 1024 1024" strokeWidth="0" width={size} height={size} xmlns="http://www.w3.org/2000/svg">
+           <path
+               strokeWidth="0"
+               d="M715.8 493.5L335 165.1c-14.2-12.2-35-1.2-35 18.5v656.8c0 19.7 20.8 30.7 35 18.5l380.8-328.4c10.9-9.4 10.9-27.6 0-37z"
+               fill="currentColor"/>
+       </svg>
+    )
+}
 
 const Icon =({name,size = 24}) => {
     const icons ={
-        pencil : PencilIcon
+        pencil : PencilIcon,
+        arrowR : ArrowIcon,
+        right : rightIcon,
     }
     const Component = icons[name]
     return <Component size={size}/>
