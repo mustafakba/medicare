e4bac80a7ff36c6fde751c52ab72af346c5c1b7a
mustafakba <kaba.mustafaa@gmail.com>
2022-12-31 00:03:43 +0300

    OurPortfolio overlay created and finished

--- src/Components/OurPortfolio.js
+++ src/Components/OurPortfolio.js
@@ -1,5 +1,6 @@
 import styles from '../Styles/OurPortfolio.css'
 import TopTitle from "./Shared/TopTitle";
+import {Icon} from "./Icons";
 
 function OurPortfolio() {
     return (
@@ -28,24 +29,83 @@ function OurPortfolio() {
             <div className="image-containers">
                 <div className="image-containers-left-section">
                     <div className="top-line-images">
-                        <div className="left-image">
+                        <div className="left-image container-relative">
+                            <div className="overlay">
+                                    <h4 className="overlay-title">
+                                        COSMETIC DENTISTRY
+                                    </h4>
+                                    <span className="overlay-content">
+                                    Health Dental
+                                    </span>
+                                        <div className="overlay-icon"><Icon size="16" name="right"/></div>
+                            </div>
                             <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.1.jpg" alt=""/>
                         </div>
-                        <div className="right-image">
+                        <div className="right-image container-relative">
+                            <div className="overlay">
+                                <h4 className="overlay-title">
+                                    COSMETIC DENTISTRY
+                                </h4>
+                                <span className="overlay-content">
+                                    Health Dental
+                                    </span>
+                                <div className="overlay-icon"><Icon size="16" name="right"/></div>
+                            </div>
                             <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.2.jpg" alt=""/>
                         </div>
                     </div>
-                    <div style={{marginTop:'-5px'}} className="left-section-bottom-image">
+                    <div style={{marginTop:'-5px'}} className="left-section-bottom-image container-relative">
+                        <div className="overlay">
+                            <h4 className="overlay-title">
+                                COSMETIC DENTISTRY
+                            </h4>
+                            <span className="overlay-content">
+                                    Health Dental
+                                    </span>
+                            <div className="overlay-icon"><Icon size="16" name="right"/></div>
+                        </div>
                         <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.5.jpg" alt=""/>
                     </div>
                 </div>
-                <div className="image-containers-right-section">
-                    <div className="right-section-left-image">
+                <div className="image-containers-right-section ">
+                    <div className="right-section-left-image container-relative">
+                        <div className="overlay">
+                            <h4 className="overlay-title">
+                                COSMETIC DENTISTRY
+                            </h4>
+                            <span className="overlay-content">
+                                    Health Dental
+                                    </span>
+                            <div className="overlay-icon"><Icon size="16" name="right"/></div>
+                        </div>
                         <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.3.jpg" alt=""/>
                     </div>
                         <div className="right-section-right-images">
-                           <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.4.jpg" alt=""/>
-                           <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.6.jpg" alt=""/>
+
+                          <div className="container-relative">
+                              <div className="overlay">
+                                  <h4 className="overlay-title">
+                                      COSMETIC DENTISTRY
+                                  </h4>
+                                  <span className="overlay-content">
+                                    Health Dental
+                                    </span>
+                                  <div className="overlay-icon"><Icon size="16" name="right"/></div>
+                              </div>
+                              <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.4.jpg" alt=""/>
+                          </div>
+                            <div className="container-relative">
+                                <div className="overlay">
+                                    <h4 className="overlay-title">
+                                        COSMETIC DENTISTRY
+                                    </h4>
+                                    <span className="overlay-content">
+                                    Health Dental
+                                    </span>
+                                    <div className="overlay-icon"><Icon size="16" name="right"/></div>
+                                </div>
+                                <img className="portfolio-image" src="https://themesflat.com/html/medicare/images/portfolio/v1.6.jpg" alt=""/>
+                            </div>
                         </div>
 
                 </div>
