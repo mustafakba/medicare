a75012ca5edbd3f08a47af6fd1637fa86292f2cd
mustafakba <kaba.mustafaa@gmail.com>
2022-12-31 01:17:48 +0300

    OurPricing componenti icerisindeki sliderler responsive hale getirildi

--- src/Components/OurPricing.js
+++ src/Components/OurPricing.js
@@ -1,14 +1,15 @@
 import styles from '../Styles/OurPricing.css'
 import {useState} from "react";
 import {Icon} from './Icons'
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
+
+import { Swiper, SwiperSlide ,} from "swiper/react";
 import "swiper/css/pagination";
 import 'swiper/css';
 
 
 
 // import required modules
-import { Pagination } from "swiper";
 import TopTitle from "./Shared/TopTitle";
 
 
@@ -56,120 +57,80 @@ function OurPricing() {
                     </div>
                 </div>
                 <Swiper
-                    pagination={{
-                        dynamicBullets: true,
+                    breakpoints={{
+                        576: {
+                            slidesPerView: 1,
+                            slidesPerGroup: 1
+                        },
+                        768: {
+                            slidesPerView: 2,
+                            slidesPerGroup: 2
+                        },
+                        1362: {
+                            slidesPerView: 3,
+                            slidesPerGroup: 3
+                        }
                     }}
-                    modules={[Pagination]}
-                    className="mySwiper"
+                    modules={[Navigation, Pagination]}
+                    spaceBetween={0}
+                    slidesPerView={1}
+                    slidesPerGroup={1}
+                    navigation
+                     pagination={{ clickable: true }}
+                     scrollbar={{ draggable: true }}
+                     onSwiper={(swiper) => console.log(swiper)}
+                     onSlideChange={() => console.log("slide change")}
                 >
-                    <SwiperSlide>
+                    {[...values, ...values].map((value,index) => (
+                        <SwiperSlide>
                            <div className="pricing-card-container">
-                               {
-                                   values.map((value,index) => {
-                                       return(
-                                           <div className="pricing-card">
-                                               <div className="pricing-image">
-                                                   <img src={value.image} alt=""/>
-                                               </div>
-                                               <div className="wrap-plan">
+                               <div className="pricing-card">
+                                   <div className="pricing-image">
+                                       <img src={value.image} alt=""/>
+                                   </div>
+                                   <div className="wrap-plan">
                                         <span className="plan">
                                             {value.title}
                                         </span>
-                                               </div>
-                                               <div className="pricing-features">
-                                                   <div className="pricing-left-side">
-                                                       {
-                                                           value.services.map((service,index) => {
-                                                               return(
-                                                                   // <div className="icon-type" key={index}>
-                                                                   //     {service === "true" ? (<Icon name="tick" ) :(<Icon name="times" />) }
-                                                                   // </div>
-                                                                   <div className="icon-type">
-                                                                       <Icon name={service === 'true' ? 'tick' : 'times'} />
-                                                                   </div>
-                                                               )
-                                                           })
-                                                       }
-                                                   </div>
-                                                   <div className="pricing-right-side">
-                                                       <li className="pricing-features-item">Diagnostic Services</li>
-                                                       <li className="pricing-features-item">Professional Consultation</li>
-                                                       <li className="pricing-features-item">Tooth Implants</li>
-                                                       <li className="pricing-features-item">Surgical Extractions</li>
-                                                       <li className="pricing-features-item">Teeth Whitening</li>
-                                                   </div>
-                                               </div>
-                                               <div className="pricing-price">
-                                                   <span className="turkish-dolar">$</span>
-                                                   <span className="price">{value.price}</span>
-                                               </div>
-                                               <div className="cta">
-                                                   <div className="pricing-button-area">
-                                                       <a href="#">GET STARTED</a>
-                                                   </div>
-                                               </div>
-                                           </div>
-                                       )
-                                   })
-                               }
+                                   </div>
+                                   <div className="pricing-features">
+                                       <div className="pricing-left-side">
+                                           {
+                                               value.services.map((service,index) => {
+                                                   return(
+                                                       // <div className="icon-type" key={index}>
+                                                       //     {service === "true" ? (<Icon name="tick" ) :(<Icon name="times" />) }
+                                                       // </div>
+                                                       <div className="icon-type">
+                                                           <Icon name={service === 'true' ? 'tick' : 'times'} />
+                                                       </div>
+                                                   )
+                                               })
+                                           }
+                                       </div>
+                                       <div className="pricing-right-side">
+                                           <li className="pricing-features-item">Diagnostic Services</li>
+                                           <li className="pricing-features-item">Professional Consultation</li>
+                                           <li className="pricing-features-item">Tooth Implants</li>
+                                           <li className="pricing-features-item">Surgical Extractions</li>
+                                           <li className="pricing-features-item">Teeth Whitening</li>
+                                       </div>
+                                   </div>
+                                   <div className="pricing-price">
+                                       <span className="turkish-dolar">$</span>
+                                       <span className="price">{value.price}</span>
+                                   </div>
+                                   <div className="cta">
+                                       <div className="pricing-button-area">
+                                           <a href="#">GET STARTED</a>
+                                       </div>
+                                   </div>
+                               </div>
                            </div>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                      <div className="relative">
-                          <div className="pricing-card-container">
-                              {
-                                  values.map((value,index) => {
-                                      return(
-                                          <div className="pricing-card">
-                                              <div className="pricing-image">
-                                                  <img src={value.image} alt=""/>
-                                              </div>
-                                              <div className="wrap-plan">
-                                        <span className="plan">
-                                            {value.title}
-                                        </span>
-                                              </div>
-                                              <div className="pricing-features">
-                                                  <div className="pricing-left-side">
-                                                      {
-                                                          value.services.map((service,index) => {
-                                                              return(
-                                                                  // <div className="icon-type" key={index}>
-                                                                  //     {service === "true" ? (<Icon name="tick" ) :(<Icon name="times" />) }
-                                                                  // </div>
-                                                                  <div className="icon-type">
-                                                                      <Icon name={service === 'true' ? 'tick' : 'times'} />
-                                                                  </div>
-                                                              )
-                                                          })
-                                                      }
-                                                  </div>
-                                                  <div className="pricing-right-side">
-                                                      <li className="pricing-features-item">Diagnostic Services</li>
-                                                      <li className="pricing-features-item">Professional Consultation</li>
-                                                      <li className="pricing-features-item">Tooth Implants</li>
-                                                      <li className="pricing-features-item">Surgical Extractions</li>
-                                                      <li className="pricing-features-item">Teeth Whitening</li>
-                                                  </div>
-                                              </div>
-                                              <div className="pricing-price">
-                                                  <span className="turkish-dolar">$</span>
-                                                  <span className="price">{value.price}</span>
-                                              </div>
-                                              <div className="cta">
-                                                  <div className="pricing-button-area">
-                                                      <a href="#">GET STARTED</a>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                      )
-                                  })
-                              }
-                          </div>
-                      </div>
-                    </SwiperSlide>
+                        </SwiperSlide>
+                        )
+                    )}
                 </Swiper>
-
             </div>
         </div>
     )
